3cd09208b3fa7fec711aa17c55b563e2c8a01421
Michael Anderson <mandersonisd@gmail.com>
2024-08-16 12:23:15 -0600

    Installed sequelize

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
 const sequelize = require('./config/connection');
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+// API Routes
+app.use('/api', routes);
 
-// sync sequelize models to the database, then turn on the server
+// Sync Sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
